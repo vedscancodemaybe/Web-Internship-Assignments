cd0755162fb574361e29a756e026e84b991ba315
vedMayCode? <165877077+vedscancodemaybe@users.noreply.github.com>
2025-06-21 13:44:02 +0530

    Created calculator.js

---
+++ DAY-6_HTML/calculator.js
@@ -0,0 +1,12 @@
+let num1 = parseFloat(prompt("Enter first number:"));
+let num2 = parseFloat(prompt("Enter second number:"));
+
+let sum = num1 + num2;
+let difference = num1 - num2;
+let product = num1 * num2;
+let quotient = num1 / num2;
+
+console.log(`Sum: ${sum}`);
+console.log(`Difference: ${difference}`);
+console.log(`Product: ${product}`);
+console.log(`Quotient: ${quotient}`);
