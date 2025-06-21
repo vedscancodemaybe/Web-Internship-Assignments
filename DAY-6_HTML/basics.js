fa6423c16810d88cd4dbde1f325c6a535f9c36de
vedMayCode? <165877077+vedscancodemaybe@users.noreply.github.com>
2025-06-21 13:42:52 +0530

    Created basics.js

---
+++ DAY-6_HTML/basics.js
@@ -0,0 +1,18 @@
+var userName = "Ved";
+let age = 20;
+const isStudent = true;
+
+let greeting = "Hello, world!";
+let number = 42;
+let isLoggedIn = false;
+let colors = ["red", "green", "blue"];
+let person = { name: "Dev", age: 02 };
+
+console.log("userName:", userName);
+console.log("age:", age);
+console.log("isStudent:", isStudent);
+console.log("greeting:", greeting);
+console.log("number:", number);
+console.log("isLoggedIn:", isLoggedIn);
+console.log("colors:", colors);
+console.log("person:", person);
