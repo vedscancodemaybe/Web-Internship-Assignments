357e1717962bade28006e18c052baabbd86ed437
vedMayCode? <165877077+vedscancodemaybe@users.noreply.github.com>
2025-06-21 13:47:12 +0530

    Create function.js

---
+++ DAY-6_HTML/function.js
@@ -0,0 +1,11 @@
+function greetUser(name) {
+    console.log(`Welcome, ${name}!`);
+}
+
+greetUser("Vedant");
+
+function square(num) {
+    return num * num;
+}
+
+console.log("Square of 6 is:", square(6));
