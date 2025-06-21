7a1ae4cc34e2ec496e6e108458db75fe499d5a2f
vedMayCode? <165877077+vedscancodemaybe@users.noreply.github.com>
2025-06-21 13:46:26 +0530

    Create loops.js

---
+++ DAY-6_HTML/loops.js
@@ -0,0 +1,11 @@
+console.log("Numbers from 1 to 20:");
+for (let i = 1; i <= 20; i++) {
+    console.log(i);
+}
+
+console.log("Even numbers from 2 to 10:");
+let i = 2;
+while (i <= 10) {
+    console.log(i);
+    i += 2;
+}
